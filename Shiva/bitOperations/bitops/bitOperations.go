81331f0b2146f55fd3ab561bb72a4dcab0d9548c
Shiva Rama Krishna <shivap@mkcl.org>
2021-04-07 21:21:27 +0530

    Added more bit operations

--- Shiva/bitOperations/bitops/bitOperations.go
+++ Shiva/bitOperations/bitops/bitOperations.go
@@ -5,9 +5,10 @@ import (
 	"math/bits"
 )
 
-//CheckLSB recieves a number as int and
-//checks Least Significant Bit (LSB) of a number using bitwise operator.
 /*
+CheckLSB recieves a number as int and
+checks Least Significant Bit (LSB) of a number using bitwise operator.
+
 To check LSB of a number we need to perform bitwise ANDing.
 The bitwise AND operation number & 1 will evaluate to 1 if LSB of number is set i.e. 1
 otherwise evaluates to 0.
@@ -25,15 +26,16 @@ Whereas, 15 & 1 evaluate to 1 since LSB of 15 is 1.
 */
 func CheckLSB(num int) {
 	if num&1 == 1 {
-		fmt.Printf("LSB of %d is set (1)", num)
+		fmt.Printf("\nLSB of %d is set (1)", num)
 	} else {
-		fmt.Printf("LSB of %d is unset (0)", num)
+		fmt.Printf("\nLSB of %d is unset (0)", num)
 	}
 }
 
-//checkMSB recieves a number as int and
-//checks Most Significant Bit (MSB) of a number using bitwise operator.
 /*
+CheckMSB recieves a number as int and
+checks Most Significant Bit (MSB) of a number using bitwise operator.
+
 Step by step descriptive logic to check MSB of a number.
 
 1. Input a number from user. Store it in some variable say num.
@@ -56,11 +58,93 @@ Step by step descriptive logic to check MSB of a number.
 		------
 */
 func CheckMSB(num uint) {
-	var b uint = 1 << (bits.Len(num) - 1)
+	bitLength := bits.Len(num)
+	var msb uint
 
-	if num&1 == 1 {
-		fmt.Printf("LSB of %d is set (1)", b)
+	if bitLength > 0 {
+		msb = 1 << (bits.Len(num) - 1)
 	} else {
-		fmt.Printf("LSB of %d is unset (0)", b)
+		msb = 0
 	}
+
+	if num&msb == 0 {
+		fmt.Printf("\nMSB of %d is unset (0)", msb)
+	} else {
+		fmt.Printf("\nMSB of %d is set (1)", msb)
+	}
+}
+
+/*
+CheckBitStatus recieves a number as int and
+check status of bit at the given position of number using bitwise operator.
+
+1. Input a number from user. Store it in some variable say num and
+ position of the bit which status has to be found as bitPosition
+
+  => Let the number be 5 (101) and position as 1 i.e. of 0
+
+2 Find number of bits used to represent the given number in memory.
+	Use bits.Len() to find the bit length.
+
+3. To get the status of bit, move first bit of 1 to highest order.
+	 Left shift 1 bitPosition-1 times and store result in some variable
+	 say msb = 1 << (bitPosition - 1)
+
+	 msb = 1 << 1 results in msb = 2 (10)
+
+4. If bitwise AND operation num & msb evaluate not equal to 0 then MSB of num is set otherwise not.
+			101
+		& 010
+		------
+		  000
+		------
+*/
+func CheckBitStatus(num uint, bitPosition int) {
+	bitStatus := num >> bitPosition
+	if bitStatus&1 == 0 {
+		fmt.Printf("\nStatus of bit at index %d of %d is unset (0)", bitPosition, num)
+	} else {
+		fmt.Printf("\nStatus of bit at index %d of %d is set (1)", bitPosition, num)
+	}
+}
+
+/*
+SetBitStatus recieves a number as int and
+set status of bit at the given position of number to 1 (set) using bitwise operator.
+
+1. Input a number from user. Store it in some variable say num and
+ position of the bit which status has to be found as bitPosition
+
+  => Let the number be 5 (101) and position as 1 i.e. of 0
+
+2. Move first bit of 1 to highest order.
+	 Left shift 1 bitPosition times
+	 1 << bitPosition => 1 << 1  = 10
+
+4. Perform bitwise OR on given number and value obtained after shifting.
+			101 - 5
+		| 010 - 2
+		------
+		  111 - 7
+		------
+*/
+func SetBitStatus(num uint, bitPosition int) {
+	newNum := 1<<bitPosition | num
+	fmt.Printf("\nNumber before setting %d bit: %d (in decimal)", bitPosition, num)
+	fmt.Printf("\nNumber after setting %d bit: %d (in decimal)", bitPosition, newNum)
+}
+
+/*
+UnsetBitStatus recieves a number as int and
+unset status of bit at the given position of number to 1 (set) using bitwise operator.
+
+1. Input number and nth bit position to clear from user. Store it in some variable say num and bitPosition.
+2. Left shift 1, n times i.e. 1 << n.
+3. Perform bitwise complement with the above result. So that the nth bit becomes unset and rest of bit becomes set i.e. ~ (1 << n).
+4. Finally, perform bitwise AND operation with the above result and num. The above three steps together can be written as num & (~ (1 << n));
+*/
+func UnsetBitStatus(num uint, bitPosition int) {
+	newNum := num & (^(1 << bitPosition))
+	fmt.Printf("\nNumber before unsetting (clear) %d bit: %d (in decimal)", bitPosition, num)
+	fmt.Printf("\nNumber after unsetting (clear) %d bit: %d (in decimal)", bitPosition, newNum)
 }
--- Shiva/bitOperations/main.go
+++ Shiva/bitOperations/main.go
@@ -7,8 +7,17 @@ import (
 func main() {
 
 	//to check the lsb
-	bitops.CheckLSB(4)
+	bitops.CheckLSB(18)
 
-	//to check the msb
-	bitops.CheckMSB(4)
+	//to check the msb of numbers 0 and greater
+	bitops.CheckMSB(0)
+
+	//to check the status of the bit at position (0-31) from right
+	bitops.CheckBitStatus(18, 0)
+
+	//to set the bit status at position of give number
+	bitops.SetBitStatus(4, 0)
+
+	//to unset(clear) the bit status at position of give number
+	bitops.UnsetBitStatus(11, 1)
 }
